0fa209d53e6e4acf066d1862caf1a9296af60315
fernando <sanchezjorgefernando@gmail.com>
2024-11-21 18:48:35 +0200

    crear rutas

---
+++ vue-project/src/components/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="sidebar">
+        <h2>Barra Lateral</h2>
+        <ul>
+            <li><router-link to="/contador">Contador</router-link></li>            
+            <li><router-link to="/lista-de-tareas">Lista de Tareas</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .sidebar {
+        width: 200px;
+        background-color: rgb(187, 174, 121);
+        padding: 20px;
+    }
+</style>
